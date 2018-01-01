24ab4b79f94eb952a10083302ecd2b3e661dfe61
Kevin Koshiol <ntarelix@gmail.com>
2018-01-01 10:24:48 -0600

    Replace uniform transformations with matrix transformation

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,7 @@ import createShader from './core/create-shader';
 import createProgram from './core/create-program';
 import vs2d from './shaders/vs-2d';
 import fsVariable from './shaders/fs-variable';
+import * as m3 from './core/m3';
 
 /**
  * Creates a right triangle
@@ -41,35 +42,33 @@ function main() {
   gl.useProgram(program);
   const positionAttr = gl.getAttribLocation(program, `a_position`);
   const colorUni = gl.getUniformLocation(program, `u_color`);
-  const resolutionUni = gl.getUniformLocation(program, `u_resolution`);
-  const rotationUni = gl.getUniformLocation(program, `u_rotation`);
-  const scaleUni = gl.getUniformLocation(program, `u_scale`);
-  const translationUni = gl.getUniformLocation(program, `u_translation`);
+  const transformUni = gl.getUniformLocation(program, `u_transform`);
   gl.enableVertexAttribArray(positionAttr);
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.vertexAttribPointer(positionAttr, 2, gl.FLOAT, false, 0, 0);
-  gl.uniform2f(resolutionUni, gl.canvas.width, gl.canvas.height);
   //render
   gl.clearColor(0, 0, 0, 1); //reset to black
   gl.clear(gl.COLOR_BUFFER_BIT);
-  const triangleSize = 25;
   for (let i = 0; i < 100; i++) {
-    setTriangle(gl, triangleSize);
+    setTriangle(gl, 25);
     gl.uniform4f(colorUni, Math.random(), Math.random(), Math.random(), 1);
-    gl.uniform2f(
-      translationUni,
-      Math.random() * (gl.canvas.width - triangleSize),
-      Math.random() * (gl.canvas.height - triangleSize)
-    );
-    const angleDeg = Math.random() * 360;
-    const angleRad = angleDeg * Math.PI / 180;
-    const rotX = Math.cos(angleRad);
-    const rotY = Math.sin(angleRad);
-    const scaleX = Math.random() * 3 + 0.5;
-    const scaleY = Math.random() * 3 + 0.5;
-    gl.uniform2f(rotationUni, rotX, rotY);
-    gl.uniform2f(scaleUni, scaleX, scaleY);
+    const translateX = Math.random() * (gl.canvas.width - 25);
+    const translateY = Math.random() * (gl.canvas.height - 25);
+    const angleInRadians = Math.random() * 2 * Math.PI;
+    const scaleX = Math.random() * 1.5 + 0.5;
+    const scaleY = Math.random() * 1.5 + 0.5;
+    const projectionMatrix = m3.projection(gl.canvas.width, gl.canvas.height);
+    const translationMatrix = m3.translation(translateX, translateY);
+    const rotationMatrix = m3.rotation(angleInRadians);
+    const scalingMatrix = m3.scaling(scaleX, scaleY);
+    const tranformationMatrix = [
+      projectionMatrix,
+      translationMatrix,
+      rotationMatrix,
+      scalingMatrix
+    ].reduce(m3.multiply);
+    gl.uniformMatrix3fv(transformUni, false, tranformationMatrix);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 }
--- src/shaders/vs-2d.js
+++ src/shaders/vs-2d.js
@@ -1,20 +1,7 @@
 export default `
   attribute vec2 a_position;
-  uniform vec2 u_resolution;
-  uniform vec2 u_rotation;
-  uniform vec2 u_scale;
-  uniform vec2 u_translation;
+  uniform mat3 u_transform;
   void main() {
-    vec2 scaledPosition = a_position * u_scale;
-    vec2 rotatedPosition = vec2(
-      scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x,
-      scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x
-    );
-    vec2 translatedPosition = rotatedPosition + u_translation;
-    vec2 zeroToOne = translatedPosition / u_resolution;
-    vec2 zeroToTwo = zeroToOne * 2.0;
-    vec2 clipSpace = zeroToTwo - 1.0;
-    vec2 flippedYAxis = clipSpace * vec2(1, -1);
-    gl_Position = vec4(flippedYAxis, 0, 1);
+    gl_Position = vec4((u_transform * vec3(a_position, 1)).xy, 0, 1);
   }
 `;
