8d38eccf076a4c824c95069c1b35e56458f40818
Kevin Koshiol <ntarelix@gmail.com>
2018-01-04 21:18:46 -0600

    Renders firefox texture multiple times with random transforms

--- src/entry.js
+++ src/entry.js
@@ -3,58 +3,20 @@ import createProgram from './core/create-program';
 import vs2d from './shaders/vs-2d';
 import fsVariable from './shaders/fs-variable';
 import * as m3 from './core/m3';
+import imageUrl from './test-images/firefox.png';
 
-/**
- * Creates a rectangle
- * @param {WebGLRenderingContext} gl
- * @param {number} width
- * @param {number} height
- */
-function setRectangle(gl, width, height) {
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      //top left
-      0,
-      0,
-      //top right
-      width,
-      0,
-      //bottom right
-      width,
-      height,
-      //bottom left
-      0,
-      height
-    ]),
-    gl.STATIC_DRAW
-  );
+async function loadImage(imageUrl) {
+  const imageObj = new Image();
+  imageObj.src = imageUrl;
+  await new Promise((resolve, reject) => {
+    imageObj.onload = resolve;
+    imageObj.onerror = reject;
+  });
+  return imageObj;
 }
 
-/**
- * Creates a right triangle
- * @param {WebGLRenderingContext} gl
- * @param {number} size
- */
-function setTriangle(gl, size) {
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      //top left
-      0,
-      0,
-      //bottom right
-      size,
-      size,
-      //bottom left
-      0,
-      size
-    ]),
-    gl.STATIC_DRAW
-  );
-}
-
-function main() {
+async function main() {
+  const image = await loadImage(imageUrl);
   /** @type {HTMLCanvasElement} */
   const canvasNode = document.querySelector(`.gameCanvas`);
   const gl = canvasNode.getContext(`webgl`);
@@ -65,34 +27,94 @@ function main() {
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vs2d);
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsVariable);
   const program = createProgram(gl, vertexShader, fragmentShader);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl.useProgram(program);
   const positionAttr = gl.getAttribLocation(program, `a_position`);
-  const colorUni = gl.getUniformLocation(program, `u_color`);
-  const transformUni = gl.getUniformLocation(program, `u_transform`);
-  gl.enableVertexAttribArray(positionAttr);
+  const textureCoordinateAttr = gl.getAttribLocation(program, `a_textureCoordinates`);
+  const transformationUni = gl.getUniformLocation(program, `u_transform`);
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.vertexAttribPointer(positionAttr, 2, gl.FLOAT, false, 0, 0);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      //top left
+      0,
+      0,
+      //top right
+      image.naturalWidth,
+      0,
+      //bottom right
+      image.naturalWidth,
+      image.naturalHeight,
+      //bottom left
+      0,
+      image.naturalHeight,
+      //top left
+      0,
+      0,
+      //top right
+      image.naturalWidth,
+      0,
+      //bottom right
+      image.naturalWidth,
+      image.naturalHeight,
+      //bottom left
+      0,
+      image.naturalHeight
+    ]),
+    gl.STATIC_DRAW
+  );
+  const textureCoordinateBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      //top left
+      0,
+      0,
+      //top right
+      1,
+      0,
+      //bottom right
+      1,
+      1,
+      //bottom left
+      0,
+      1
+    ]),
+    gl.STATIC_DRAW
+  );
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
   //render
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.clearColor(0, 0, 0, 1); //reset to black
   gl.clear(gl.COLOR_BUFFER_BIT);
-  for (let i = 0; i < 100; i++) {
-    // setTriangle(gl, 25);
-    setRectangle(gl, 20, 50);
-    gl.uniform4f(colorUni, Math.random(), Math.random(), Math.random(), 1);
-    const translateX = Math.random() * (gl.canvas.width - 20);
-    const translateY = Math.random() * (gl.canvas.height - 50);
-    const angleInRadians = Math.random() * 2 * Math.PI;
-    const scaleX = Math.random() * 1.5 + 0.5;
-    const scaleY = Math.random() * 1.5 + 0.5;
+  gl.useProgram(program);
+  gl.enableVertexAttribArray(textureCoordinateAttr);
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
+  gl.vertexAttribPointer(textureCoordinateAttr, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionAttr);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionAttr, 2, gl.FLOAT, false, 0, 0);
+  for (let i = 0; i < 5; i++) {
     const projectionMatrix = m3.projection(gl.canvas.width, gl.canvas.height);
-    const translationMatrix = m3.translation(translateX, translateY);
-    const rotationMatrix = m3.rotation(angleInRadians);
-    const scalingMatrix = m3.scaling(scaleX, scaleY);
-    const tranformationMatrix = [projectionMatrix, translationMatrix, rotationMatrix, scalingMatrix].reduce(m3.multiply);
-    gl.uniformMatrix3fv(transformUni, false, tranformationMatrix);
-    // gl.drawArrays(gl.TRIANGLES, 0, 3);
+    const transX = Math.random() * (gl.canvas.width - 2 * image.naturalWidth) + image.naturalWidth;
+    const transY = Math.random() * (gl.canvas.height - 2 * image.naturalHeight) + image.naturalHeight;
+    const translationMatrix = m3.translation(transX, transY);
+    const scale = Math.random() * 1.8 + 0.2
+    const scalingMatrix = m3.scaling(scale, scale);
+    const rotationMatrix = m3.rotation(Math.random() * 2 * Math.PI);
+    const transformationMatrix = [
+      projectionMatrix,
+      translationMatrix,
+      scalingMatrix,
+      rotationMatrix
+    ].reduce(m3.multiply);
+    gl.uniformMatrix3fv(transformationUni, false, transformationMatrix);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
   }
 }
--- src/shaders/vs-2d.js
+++ src/shaders/vs-2d.js
@@ -1,7 +1,10 @@
 export default `
   attribute vec2 a_position;
+  attribute vec2 a_textureCoordinates;
   uniform mat3 u_transform;
+  varying vec2 v_textureCoordinates;
   void main() {
     gl_Position = vec4((u_transform * vec3(a_position, 1)).xy, 0, 1);
+    v_textureCoordinates = a_textureCoordinates;
   }
 `;
