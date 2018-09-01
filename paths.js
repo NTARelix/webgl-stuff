608f4ab9dd356accda33208c198e4b9f582a2508
Kevin Koshiol <kevin.koshiol@calabrio.com>
2018-09-01 11:42:07 -0500

    Upgrade dependencies and transpile

--- paths.js
+++ paths.js
@@ -6,7 +6,7 @@ paths.src = path.resolve(paths.root, 'src')
 paths.nodeModules = path.resolve(paths.root, 'node_modules')
 paths.pixiDist = path.resolve(paths.nodeModules, 'pixi.js/dist/pixi.min.js')
 paths.entry = path.resolve(paths.src, 'entry.js')
-paths.demoHtml = path.resolve(paths.src, 'demo.html')
+paths.demoHtml = path.resolve(paths.src, 'index.html')
 paths.dist = path.resolve(paths.root, 'dist')
 
 module.exports = paths
